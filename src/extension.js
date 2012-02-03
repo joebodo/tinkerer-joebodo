6a4f15d313472b5ee3f91fa4ee98a8189b9ea723
joe <joebodo@gmail.com>
2012-02-03 07:50:16 -0500

    cleanup

--- src/extension.js
+++ src/extension.js
@@ -1,3 +1,24 @@
+/*
+ *  Tinkerer for GNOME Shell
+ *  - Icon for restarting Gnome Shell
+ *  - Icon for reloading the current theme
+ *  - Icon for displaying LookingGlass
+ *  
+ * The purpose of this extension is to help speed the process of writing
+ * shell extensions or creating themes.
+ *
+ * tinkerer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tinkerer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * GNU General Public License: see <http://www.gnu.org/licenses/>
+ */
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Gettext = imports.gettext.domain('gnome-shell');
